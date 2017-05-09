9968c6c83c74da6d9a242c10c1c6604b61805d32
Julien Lamarche <julien.lamarche@gmail.com>
2017-05-08 23:30:21 -0400

    created update lat long and 'best investment search'

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,10 +1,15 @@
+
+require 'nokogiri'
+require 'open-uri'
+require_relative './../../config.rb'
+
 class Property < ActiveRecord::Base
 
     def ==(another_property)
         self.address == another_property.address
     end
 
-    def self.slurpData(pathToHtml)
+    def self.slurpData(pathToHtml, city)
         dataPath=pathToHtml
 
         doc = Nokogiri::HTML(open(dataPath))
@@ -37,6 +42,8 @@ class Property < ActiveRecord::Base
                     lp = lpStr.text.gsub(/\D/, '').to_i
                     address = address.text
 
+                    address = address + ', ' + city
+
                     property = Property.find_by(:address=>address)
 
                     if property.nil?
@@ -53,4 +60,67 @@ class Property < ActiveRecord::Base
         }
 
     end
+
+
+    def self.updateLatLong(limit = 5)
+        Property.where(:latitude => nil, :longitude => nil)[0 .. limit].each { |p|
+
+            address = p.address
+            unitNumber = address[/#[0-9]*/]
+
+            if not unitNumber.nil?
+                address = address.sub(unitNumber, '')
+                address = address.sub(' ,', ',')
+            end
+
+            doubleAddress = address[/[0-9]*&[0-9]*/]
+            if not doubleAddress.nil?
+                address = address.sub(doubleAddress, doubleAddress.split('&')[0])
+            end
+
+            url="https://maps.googleapis.com/maps/api/geocode/json?key=#{$apiKey}&address=#{address.tr(' ', '+')}"
+            resultsHash =  JSON.parse(open(url).read)
+
+            lat  = resultsHash['results'][0]['geometry']['location']['lat'].to_f
+            long = resultsHash['results'][0]['geometry']['location']['lng'].to_f
+
+            if($minLat < lat and lat < $maxLat and $minLong < long and long < $maxLong)
+                p.update(:latitude=>lat, :longitude=>long)
+            else
+                $stderr.puts "Invalid lat long for #{address} (#{lat}, #{long}) (#{url})"
+            end
+            sleep 1
+        }
+
+    end
+
+
+    def averageRent()
+        long = self.longitude
+        lat  = self.latitude
+        results=`cd #{$tifDir}; for tifFile in heatMapTIN.tif heatMapIDW.tif; do  gdallocationinfo -wgs84  $tifFile #{long} #{lat} | grep Value | cut -d ':' -f 2 ; done`
+
+        sumRentPerRoom = 0
+        numDataPoints = 0
+        results.each_line {|l| sumRentPerRoom += l.strip.to_i; numDataPoints += 1}
+
+        avgRentPerRoom = (sumRentPerRoom / numDataPoints).round(2)
+    end
+
+
+    def rentPerLpRatio()
+        rentPerLPRatio = (self.averageRent.to_f * self.bedrooms / self.listingPrice)*100.round(2)
+        rentPerLPRatio.round(2).to_f
+    end
+
+    def self.bestInvestments()
+        Property.all.select{ |p| p.rentPerLpRatio >= 0.7} \
+            .sort_by{|p| p.rentPerLpRatio}.reverse \
+            .each {|p|
+            puts "#{p.address}\t#{p.rentPerLpRatio}\t#{p.listingPrice}\t#{p.bedrooms}"
+            }
+    end
+
+
+
 end
